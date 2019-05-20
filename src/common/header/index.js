75f5c098e56e4fdcd688a44510fd37dfa800688b
pheromone <510738319@qq.com>
2019-05-20 14:28:00 +0800

    add 基本的header
    add 基本的header

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HeaderWrapper,Logo,Nav } from './style';
+import { HeaderWrapper,Logo,Nav,NavItem,NavSearch,Addition,Button } from './style';
 
 class Header extends Component {
     render() {
@@ -7,8 +7,16 @@ class Header extends Component {
         <HeaderWrapper>
             <Logo />
             <Nav>
-
+              <NavItem className='left active'>首页</NavItem>
+              <NavItem className='left'>下载App</NavItem>
+              <NavItem className='right'>登录</NavItem>
+              <NavItem className='right'>Aa</NavItem>
+              <NavSearch></NavSearch>
             </Nav>
+            <Addition>
+              <Button className="writting">写文章</Button>
+              <Button className="reg">注册</Button>
+            </Addition>
         </HeaderWrapper>
       );
     }
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -24,6 +24,68 @@ export const Logo = style.a.attrs({
 export const Nav = style.div`
   width:960px;
   height:100%;
+  padding-right: 70px;
+  box-sizing:border-box;
   margin: 0 auto;
-  background:green;
+`
+
+export const NavItem = style.div`
+  line-height:56px;
+  padding: 0 15px;
+  color: #333333;
+  &.left{
+      float:left;
+  }
+   &.right{
+      float:right;
+      color:#969696;
+  }
+  &.active{
+      color: #ea6f5a;
+  }
+`
+
+export const NavSearch = style.input.attrs({
+   placeholder:'搜索'
+})`
+   padding:0 20px;
+   box-sizing: border-box;
+   margin-top:9px;
+   width:160px;
+   height:38px;
+   border:none;
+   outline:none;
+   border-radius:19px;
+   background:#eee;
+   font-size:14px;
+   margin-left:20px;
+   &::placeholder{
+       color:#999;
+   }
+`
+
+export const Addition = style.div`
+    position:absolute;
+    right:0;
+    top:0;
+    height:56px;
+
+`
+
+export const Button = style.div`
+  float: right;
+  margin-top: 9px;
+  margin-right: 20px;
+  padding: 0 20px;
+  line-height: 38px;
+  border-radius: 19px;
+  border: 1px solid #ec6149;
+  font-size:14px;
+  &.reg{
+      color:#ec6149;
+  }
+  &.writting{
+     color: #fff;
+     background: #ec6149;
+  }
 `
